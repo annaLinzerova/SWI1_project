7ca56a6ff28c2375bfeb6520349c83ce09e98314
annaLinzerova <annalinzerova@seznam.cz>
2026-05-01 10:50:02 +0200

    Adding project to GitHub

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,35 @@
+import './LoginPage.css'
+import React from 'react'
+import { Link } from "react-router-dom"
+
+const MyComponent = () => {
+    return(
+        <Link to="/RegisterPage">Register Page</Link>
+    )
+}
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<body>
+<img src="images/ver1SWIbckg.jpg" class="backimg" alt="Background image" />
+<section class="velkyObdelnik">
+<h1>Login Page</h1>
+
+<form name="formularPrihlasovani"  action="form.html" method="post">
+
+  <p><label  class="login" for="item1">username:  </label>
+  <input id="item1" class="policko"  name="item1" type="text"></p>
+  
+  <p><label  class="login" for="item2">password:  </label>
+  <input id="item2"  class="policko" name="item2" type="text"></p>
+</form>
+<a href="/RegisterPage.tsx"><button id="loginButton" class="loginButton" type="button"><a>Login</a></button></a>
+{<link to="/RegisterPage">Texttttttttt</link>}
+
+<p>If you aren´t registered yet, please click <a href="/RegisterPage.tsx" class ="registerlink">here</a></p>
+    
+    
+</section>
+  
+</body>
+`
+
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,26 @@
+import './RegisterPage.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+    <body>
+    <section class="velkyObdelnik">
+        <h1>Register Page</h1>
+
+        <form name="formularRegistrace"  action="form.html" method="post">
+
+            <p><label  class="registr" for="item1">Choose your username:  </label>
+                <input id="item1" class="policko"  name="item1" type="text"></p>
+
+            <p><label  class="registr" for="item2">Choose a strong password:  </label>
+                <input id="item2"  class="policko" name="item2" type="text"></p>
+
+            <p><label  class="registr" for="item2">Please repeat password:  </label>
+                <input id="item3"  class="policko" name="item3" type="text"></p>
+        </form>
+        <a href="/LoginPage.tsx"><button id="registerButton" class="registerButton" type="button"><a>Register</a></button></a>
+
+        <p>If you aren´t registered yet, please click <a href="/RegisterPage.tsx" class ="registerlink">here</a></p>
+
+
+    </section>
+    </body>
+`
